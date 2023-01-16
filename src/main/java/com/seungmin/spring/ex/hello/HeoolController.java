fdc947db19de412e44485bea2010fcaf4dd4f6fd
이승민 <harry351111@naver.com>
2023-01-16 20:46:39 +0900

    [이승민] 최초 파일 추가

---
+++ src/main/java/com/seungmin/spring/ex/hello/HeoolController.java
@@ -0,0 +1,17 @@
+package com.seungmin.spring.ex.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HeoolController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+
+}
